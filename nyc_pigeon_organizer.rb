8e487c9ee7dbfe2df06a595ccfb638e2299a62ef
Roberto Mejia <rmeji1@unh.newhaven.edu>
2019-08-27 22:28:57 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,26 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_data = {}
+  data.each_pair do |(attribute, attribute_value)|
+    evaluate_attribute_value_for(attribute, attribute_value, new_data)
+  end
+  new_data
+end
+
+def evaluate_attribute_value_for(attribute, attribute_value, new_data)
+  attribute_value.each_pair do |(attribute_desc, names)|
+    evaluate_pigeon_names_for(names, attribute_desc, attribute, new_data)
+  end
+end
+
+def evaluate_pigeon_names_for(names, attribute_desc, attribute, new_data) 
+  names.each do |name| 
+    if !new_data[name] 
+      new_data[name] = {
+        :color => [],
+        :gender => [],
+        :lives => []
+      }
+    end
+    new_data[name][attribute] << attribute_desc.to_s
+  end
 end
