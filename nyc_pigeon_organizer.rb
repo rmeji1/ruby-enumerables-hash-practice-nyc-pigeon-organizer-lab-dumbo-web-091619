dac8a0433ff290676e078033168c10839842e01d
Roberto Mejia <rmeji1@unh.newhaven.edu>
2019-08-27 22:25:17 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -17,9 +17,7 @@ end
 def evaluate_pigeon_names_for(names, attribute_desc, attribute, new_data) 
   names.each do |name| 
     if !new_data[name] || !new_data[name][attribute]
-      new_data[name] = {
-        attribute => []
-      }
+      new_data[name][attribute] = []
     end
     new_data[name][attribute] << attribute_desc.to_s
   end
