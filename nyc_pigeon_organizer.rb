d32e3e45c8f326cd01fbcfcde5f3b63ad152564a
Roberto Mejia <rmeji1@unh.newhaven.edu>
2019-08-27 21:59:51 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -11,7 +11,6 @@ def evaluate_parent_value_for(parent_key, parent_value)
   parent_value.each_pair do |(child_key, names)|
     evaluate_pigeon_names_for(names, child_key, parent_key, new_data)
     puts new_data
-    break
   end
 end
 
