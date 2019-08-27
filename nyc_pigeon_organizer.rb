fe8e334ef50d9d14949f8681fa3d092bad1add96
Roberto Mejia <rmeji1@unh.newhaven.edu>
2019-08-27 21:36:06 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,6 @@
 def nyc_pigeon_organizer(data)
   data.each_pair do |(parent_key, value)|
-    p key, value
+    p parent_key, value
     puts " "
     value.each_pair do |(key, value)|
       p   key, value
