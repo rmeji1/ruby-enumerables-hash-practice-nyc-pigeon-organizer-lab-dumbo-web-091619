235ad39ea10bb3d71fcee1dd7c1f9c966874ca91
Roberto Mejia <rmeji1@unh.newhaven.edu>
2019-08-27 22:14:53 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,14 +3,14 @@ def nyc_pigeon_organizer(data)
   data.each_pair do |(attribute, attribute_value)|
     evaluate_attribute_value_for(attribute, attribute_value, new_data)
   end
-  new_data
 end
 
-def evaluate_attribute_value_for(attribute, attribute_value, new_data)
-  new_data = new_data
+def evaluate_attribute_value_for(attribute, attribute_value)
+  new_data = {}
   attribute_value.each_pair do |(attribute_desc, names)|
     evaluate_pigeon_names_for(names, attribute_desc, attribute, new_data)
   end
+  puts new_data
 end
 
 def evaluate_pigeon_names_for(names, attribute_desc, attribute, new_data) 
