bd34db3f58ca92d7e4cefc1bdaf92cfe5e578daa
Roberto Mejia <rmeji1@unh.newhaven.edu>
2019-08-27 22:20:05 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,7 @@ def nyc_pigeon_organizer(data)
   new_data = {}
   data.each_pair do |(attribute, attribute_value)|
     evaluate_attribute_value_for(attribute, attribute_value, new_data)
-    break
+    # break
   end
 end
 
