9acbfc69500748277c88f229ef220f028b524464
Roberto Mejia <rmeji1@unh.newhaven.edu>
2019-08-27 21:32:50 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,9 +2,9 @@ def nyc_pigeon_organizer(data)
   data.each_pair do |(key, value)|
     p key, value
     
-    value.reduce({}) do |memo, val|
-      p   val
-    end
+    # value.reduce({}) do |memo, val|
+    #   p   val
+    # end
     
     break
   end
